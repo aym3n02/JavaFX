9829d78db855eacdb67a2c5b07bddab304ad8bec
Ayman <aymenchaterre@gmail.com>
2022-11-19 15:12:01 +0100

    added some stuff to test it

--- src/App.java
+++ src/App.java
@@ -5,7 +5,10 @@ import javafx.stage.Stage;
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
-        launch(args);
+        launch(args);     
+        
+        // blabla testing
+
     }
 
     @Override
